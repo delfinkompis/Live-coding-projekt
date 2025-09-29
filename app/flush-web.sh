81b3c31326c4f58afc17cffde2ae5939ad212d5b
Hjalmar Bjerner <hjalmar@bjerner.net>
2025-09-29 14:14:51 +0200

    f
    	ändrad:        app/flush-web.sh

--- app/flush-web.sh
+++ app/flush-web.sh
@@ -3,6 +3,7 @@ cp speech-template.wav input-speech.wav
 cp speech-template.wav speech-scaled.wav
 cp lower-template.ily lower.ily
 cp upper-template.ily upper.ily
+cp middle-template.ily middle.ily
 cp input-template.txt input.txt
 cp input-template.txt timestamps.txt
 cp input-template.txt log.txt
