6bdfee476b1d3a5a0fce94570779d9b381531e07
Hjalmar Bjerner <hjalmar@bjerner.net>
2025-09-29 13:36:54 +0200

    flusher output-lyd
    	ändrad:        app/flush-web.sh

--- app/flush-web.sh
+++ app/flush-web.sh
@@ -5,7 +5,8 @@ cp lower-template.ily lower.ily
 cp upper-template.ily upper.ily
 cp input-template.txt input.txt
 cp input-template.txt timestamps.txt
-cp input-template.txt log.txt 
+cp input-template.txt log.txt
+cp output-template.wav output.wav
 cp ly-display-template.pdf ly-display.pdf
 cp ly-display-template.svg ly-display.svg
 
