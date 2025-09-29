9b4f0aad6640fb3ab4de314676b2e4b25eb8edbd
Hjalmar Bjerner <hjalmar@bjerner.net>
2025-09-29 13:14:28 +0200

    added temp svg file to flush previous ouput when checking in
    	ändrad:        app/flush-web.sh
    	ny fil:        app/ly-display-template.svg
    	ändrad:        app/timestamps.txt

--- app/flush-web.sh
+++ app/flush-web.sh
@@ -1,10 +1,12 @@
 cp speech-template.wav speech.wav
 cp speech-template.wav input-speech.wav
+cp speech-template.wav speech-scaled.wav
 cp lower-template.ily lower.ily
 cp upper-template.ily upper.ily
 cp input-template.txt input.txt
 cp input-template.txt timestamps.txt
 cp input-template.txt log.txt 
 cp ly-display-template.pdf ly-display.pdf
+cp ly-display-template.svg ly-display.svg
 
 echo "renset alle filer"
